069c6df5899b8c485db268ef014a59ccf0fcd900
jingtingEmmaLi <31930165+jingtingEmmaLi@users.noreply.github.com>
2023-01-30 21:16:41 +0800

    demo for depth folw computation

---
+++ depthFlowComputation/demo.m
@@ -0,0 +1,30 @@
+clear; clc;
+filepath = './input/'; 
+list = dir([filepath,'*.mat']);
+for ii = 1:length(list)
+    savepath = ['./output/', list(ii).name];
+    if ~exist(savepath,'file')
+        disp(list(ii).name)
+        load([filepath,list(ii).name]);
+        onsetG = gpuArray(onset);
+        apexG = gpuArray(apex);
+        clear onset apex
+        [ux,uy,uz]=LKPR3D(onsetG,apexG,4,2,0,1);
+        % Enhance the quiver plot visually by downsizing vectors  
+        %   -f : downsizing factor
+    %     f=5;
+    %     x=ux(1:f:size(ux,1),1:f:size(ux,2),1:f:size(ux,3)); 
+    %     y=uy(1:f:size(ux,1),1:f:size(ux,2),1:f:size(ux,3)); 
+    %     z=uz(1:f:size(ux,1),1:f:size(ux,2),1:f:size(ux,3)); 
+    % 
+    %     [X,Y,Z]=meshgrid(1:size(x,2),1:size(x,1),1:size(x,3));
+    %     quiver3(X,Y,Z,x,y,z); axis([1 size(x,2) 1 size(x,1) 1 size(x,3)]);
+
+    %%
+
+        save(savepath,'ux','uy','uz');
+        disp('saved')
+
+        clear onsetG apexG ux uy uz
+    end
+end
